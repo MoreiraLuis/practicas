12b5e10d3ead21e88cf2034a477e779368e80b35
LuisMoreira <luismanuelmoreiraesteban@gmail.com>
2023-05-04 14:23:54 +0200

    mejora del código

--- js/objetos.js
+++ js/objetos.js
@@ -1,4 +1,4 @@
-function Luis(nombre, apellido, email, edad, contraseña, telefono, DNI, direccion, cp) {
+function alumno(nombre, apellido, email, edad, contraseña, telefono, DNI, direccion, cp) {
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
@@ -10,7 +10,10 @@ function Luis(nombre, apellido, email, edad, contraseña, telefono, DNI, direcci
     this.cp = cp;
   }
   
-  var Luis = new Luis("Luis", "Moreira", "luismoreira@ejemplo.es", 30, 1234, 622052509, 04725525, "Av. Dr. Ángel Pascual Megías", 03015);
+  var Luis = new alumno("Luis", "Moreira", "luismoreira@ejemplo.es", 30, 1234, 622052509, 04725525, "Av. Dr. Ángel Pascual Megías", 03015);
+  var Jesus = new alumno("Jesús", "Alonso", "jesusalonso@ejemplo.es", 27, 1234, 611215496, 24869471, "Av. Dr. Rico Pérez", 03014);
+  var Maria = new alumno("María", "Rodríguez", "mariarodriguez@ejemplo.es", 24, 1234, 620304050, 04896214, "Av. Rambla", 03010);
+
 
   function Asignaturas(Nombre, id_profesor1, id_profesor2) {
     this.Nombre = Nombre;
@@ -18,7 +21,9 @@ function Luis(nombre, apellido, email, edad, contraseña, telefono, DNI, direcci
     this.id_profesor2 = id_profesor2;
   }
 
-  var Asignaturas = new Asignaturas("Juan Pérez", 1, 2);
+  var LuisAsignaturas = new Asignaturas("Jorge Solís", 1, 2);
+  var JesusAsignaturas = new Asignaturas("Jorge Solís", 1, 2);
+  var MariaAsignaturas = new Asignaturas("Jorge Solís", 1, 2);
 
   function Notas(id_alumno, id_asignatura, id_curso, nota) {
     this.id_alumno = id_alumno;
@@ -27,7 +32,9 @@ function Luis(nombre, apellido, email, edad, contraseña, telefono, DNI, direcci
     this.nota = nota;
   }
 
-  var Notas = new Notas("Juan Pérez", "Matemáticas", "ESO", 15);
+  var LuisNotas = new Notas("Luis Moreira", "Matemáticas", "ESO", 15);
+  var JesusNotas = new Notas("Jesús Alonso", "Matemáticas", "ESO", 10);
+  var MariaNotas = new Notas("María Rodríguez", "Matemáticas", "ESO", 19);
 
   function Cursos(id_1, id_2, id_3, id_4, id_5, id_6, id_7, id_8, id_9, id_10) {
     this.id_1 = id_1;
@@ -42,7 +49,13 @@ function Luis(nombre, apellido, email, edad, contraseña, telefono, DNI, direcci
     this.id_10 = id_10;
   }
 
-  var Cursos = new Cursos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  var LuisCursos = new Cursos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  var MariaCursos = new Cursos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  var JesusCursos = new Cursos(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-  const array = [Luis, Asignaturas, Notas, Cursos];
-  console.log(array);
+  //array para mostrar datos de Luis
+  const datosDeLuis = [Luis, LuisAsignaturas, LuisNotas, LuisCursos];
+  //array para mostrar datos de Maria
+  const datosDeMaria = [Maria, MariaAsignaturas, MariaNotas, MariaCursos];
+  //array para mostrar datos de Jesus
+  const datosDeJesus = [Luis, JesusAsignaturas, JesusNotas, JesusCursos];
